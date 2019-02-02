493f7beb70ab7d7687fdd3748884b243d09298ed
gendo <gendo@evidentid.com>
2019-02-02 16:08:58 -0500

    PRODUCT-5909: iOS: Demo Project:
    - Initial app

---
+++ AssureSDK Demo/AssureSDK Demo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AssureSDK Demo
+//
+//  Created by Krisna Gendo on 2/2/19.
+//  Copyright © 2019 Evident ID. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
